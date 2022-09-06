7d3fbf2d9ee1c49d0e84edafc0f843d22d078fa8
Mike Harrison <mikeha_ca@yahoo.com>
2022-09-06 08:29:57 -0600

    fixed rounding and image multiply and opacity

--- components/StatsPreviewCard.tsx
+++ components/StatsPreviewCard.tsx
@@ -1,19 +1,19 @@
 function StatsPreviewCard() {
     return (
         <main className="flex flex-col md:flex-row-reverse text-white w-1/2 my-10 md:w-2/3 md:max-w-screen-md">
-            <div className="bg-softViolet rounded-t-2xl md:rounded-t-none md:rounded-r-sm md:flex-1">
+            <div className="bg-softViolet rounded-t-2xl md:rounded-t-none md:rounded-r-2xl md:flex-1">
                 <img
                     src="/image-header-mobile.jpg"
-                    className="mix-blend-hard-light rounded-t-2xl md:hidden"
+                    className="mix-blend-multiply opacity-75 rounded-t-2xl md:hidden"
                     alt="image"
                 />
                 <img
                     src="/image-header-desktop.jpg"
-                    className="hidden mix-blend-hard-light rounded-t-2xl md:h-full md:flex md:rounded-r-sm md:object-cover"
+                    className="hidden mix-blend-multiply opacity-75 rounded-t-2xl md:h-full md:flex md:rounded-r-2xl md:object-cover"
                     alt="image"
                 />
             </div>
-            <div className="bg-darkDesaturatedBlue p-10 text-center rounded-b-2xl md:rounded-none md:flex-1 md:text-left">
+            <div className="bg-darkDesaturatedBlue p-10 text-center rounded-b-2xl md:rounded-l-2xl md:rounded-br-none md:flex-1 md:text-left">
                 <h2 className="text-xl md:text-2xl font-bold">
                     Get <span className="text-softViolet">insights</span> that
                     help your business grow.
